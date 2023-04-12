d91d748f943c7b9bae57ddf8e8e3c1097d107d21
Elena Galkina <algalkinaevgen14@gmail.com>
2023-04-12 11:55:20 +0300

    action js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ function blockWidth() {
   w++;
   document.querySelector(".out-1").style.width = w + "px";
 }
+// alert('hello');
+// alert('hello'); 
+// alert('hello');
+
+function sumOfNumbers() {
+    let x = 6;
+    let y = 10;
+    console.log(x * y);
+    
+}
+sumOfNumbers();
+console.info('hhh');
+
+document.getElementById('out').innerHTML = 'Hello';
+document.getElementById('out').innerHTML = 2020;
+document.getElementById('out').innerHTML = 'Hello';
+document.getElementById('out').innerHTML = '<b>123</b>';
+
+document.querySelector('.header').innerHTML = 'more';
+document.querySelector('#one').innerHTML = 777;
+document.getElementById('one').innerHTML = 888;
+
+let a = document.querySelector('#one');
+a.innerHTML = 999;
+
+let b = document.querySelector('.header');
+b.innerHTML = 444;
+
+alert('hello');
