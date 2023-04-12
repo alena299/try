75a58491a9b71dcb990a77c1804b7c04ed23f3f5
Elena Galkina <algalkinaevgen14@gmail.com>
2023-04-12 09:09:22 +0300

    add js code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('hello')
+console.log("hello");
+let c = 9;
+function count () {
+  c++;
+  console.log(c);
+ 
+}
+count();
+document.querySelector(".out-1").addEventListener("mousemove", blockWidth);
+let w = 100;
+
+function blockWidth() {
+  w++;
+  document.querySelector(".out-1").style.width = w + "px";
+}
